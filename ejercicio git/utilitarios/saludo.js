a2360ad12d50a37bc8688b1f94967a10b53645b7
prisquete786-ai <prisquete786@gmail.com>
2025-10-28 17:38:15 -0500

    cambios realizados

--- ejercicio git/utilitarios/saludo.js
+++ ejercicio git/utilitarios/saludo.js
@@ -1,10 +1,19 @@
 saludar = function () {
-//recuptar el valor de la caja de texto txtNombre
-let nombre=recuperarTexto("textNombre");
-//recuptar el valor de la caja de texto txtApellido
-let pellido=recuperarTexto("textApellido")
-console.log(nombre);
+    //recuptar el valor de la caja de texto txtNombre
+    let nombre = recuperarTexto("textNombre");
+    //recuptar el valor de la caja de texto txtApellido
+    let pellido = recuperarTexto("textApellido");
+    console.log(nombre);
+
+    let Edad = recuperarInt("textEdad");
+
+
+    let Estatura = recuperarfloat("textEstatura");
+
+
 }
+
+
 
 recuperarTexto = function (idComponente) {
     let Componente;
@@ -12,4 +21,19 @@ recuperarTexto = function (idComponente) {
     Componente = document.getElementById(idComponente);
     valorIgresado = Componente.value;
     return valorIgresado;
+}
+
+recuperarInt = function (idComponente) {
+    let valorcaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorcaja);
+    return valorEntero;
+
+}
+
+
+recuperarfloat = function (idComponente) {
+    let valorcaja = recuperarTexto(idComponente);
+    let valorflotante = parseFloat(valorcaja);
+    return valorflotante;
+
 }
